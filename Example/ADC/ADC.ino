75150eb8e725a7236b2ea59875e9a41e28853d04
Makerfabs <>
2023-03-24 15:42:05 +0800

    Using for ESP32-S3

--- Example/ADC/ADC.ino
+++ Example/ADC/ADC.ino
@@ -1,18 +1,20 @@
-
-
-const int potPin = A2;
-
-// 用于存储电容湿度的变量
-int potValue = 0;
+#define ADC_PIN 17 
 
 void setup() {
-  Serial.begin(9600);
-  delay(1000);
+  Serial.begin(9600); // 
 }
 
 void loop() {
-  // 读取湿度计值
-  potValue = analogRead(potPin);
-  Serial.println(potValue);
-  delay(500);
+  int value = 0;
+  int sum = 0;
+
+  for (int i = 0; i < 5; i++) { // sampling 5 times
+    value = analogRead(ADC_PIN); // read the ADC value
+    sum += value; // Accumulates the ADC value
+    delay(1); // 
+  }
+
+  int sensorValue = sum / 5; // averaging
+  Serial.println(sensorValue); // 
+  delay(1000); // 
 }
